e8581d9a5903d843941e7b17ee9e91af49692387
SparrowSparrow <olchabolgar9886@gmail.com>
2016-04-26 11:17:38 +0300

    Ruby filling in layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | CSE"
+    assert_select "title", "CSE"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | CSE"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | CSE"
   end
 
 end
